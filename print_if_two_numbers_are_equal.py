c6455b85bc992e149100691ce776b95e2ca0de78
IzaTomacruz <izamarietomacruz0@gmail.com>
2025-03-13 12:23:20 +0800

    fix variable names for print_if_two_numbers_are_equal

---
+++ print_if_two_numbers_are_equal.py
@@ -0,0 +1,8 @@
+print("Please enter Two numbers") 
+num1 = float(input("First number: ")) 
+num2 = float(input("Second number: "))
+
+if num1 == num2:
+    print("equal")
+else:
+    print("not equal")
