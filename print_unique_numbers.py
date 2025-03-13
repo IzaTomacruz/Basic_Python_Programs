d56f6d6b6409ff23d647c8cb724e8ed1fba9c40a
IzaTomacruz <izamarietomacruz0@gmail.com>
2025-03-13 21:23:28 +0800

    algorithm for program prints numbers that doesnt have a duplicate

---
+++ print_unique_numbers.py
@@ -0,0 +1,9 @@
+# Prog01: Create a program that ask user to input 10 numbers. Display all numbers that don't have duplicate.
+
+# Create a list where we will store our numbers
+
+# Ask users to input 10 numbers
+
+# Find numbers that dont have duplicate from the list
+
+# print the numbers that dont have duplicate
