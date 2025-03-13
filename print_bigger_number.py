e006c11987237d32493033c562e104b8486bc46d
IzaTomacruz <izamarietomacruz0@gmail.com>
2025-03-13 12:03:55 +0800

    rewriten the code to follow the python coding standards, where the program asks users to input 2 numbers so that it prints the bigger number

---
+++ print_bigger_number.py
@@ -0,0 +1,10 @@
+print("Please enter Two numbers") 
+num1 = float(input("First number: ")) 
+num2 = float(input("Second number: "))
+
+if num1 > num2:
+    print("The bigger number is", num1)
+elif num1 < num2:
+    print("The bigger number is", num2)
+else:
+    print("Both are equal")
